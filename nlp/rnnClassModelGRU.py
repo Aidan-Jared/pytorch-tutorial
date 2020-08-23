2de372cb24553c4b06b71331a177aa9fd0f252d6
Aidan-Jared <AidanJared42@gmail.com>
2020-08-23 08:29:29 -0600

    minor change to nn

--- nlp/rnnClassModelGRU.py
+++ nlp/rnnClassModelGRU.py
@@ -7,16 +7,15 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.input_size = input_size
-        self.input_2_input = nn.GRU(input_size, hidden_size)
-        self.input_2_output = nn.GRU(hidden_size, hidden_size)
+        self.input_2_output = nn.GRU(input_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
         input = input.view(1,1,self.input_size)
-        output, hidden = self.input_2_input(input, hidden)
-        output, hidden = self.input_2_output(output, hidden)
-        output = F.relu(output[:,0])
+        output, hidden = self.input_2_output(input, hidden)
+        output = output.view(1,-1)
+        output = F.relu(output) # output[:,0]
         output = self.out(output)
         output = self.softmax(output)
         return output, hidden
