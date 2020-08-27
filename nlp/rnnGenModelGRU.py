283e7d3e59c827995b321c01965e9a43561568fd
Aidan-Jared <46360020+Aidan-Jared@users.noreply.github.com>
2020-08-27 15:15:22 -0600

    added linear layer and made model more readable

--- nlp/rnnGenModelGRU.py
+++ nlp/rnnGenModelGRU.py
@@ -8,9 +8,9 @@ class RNN(nn.Module):
         self.input_size = input_size
         self.category_size = n_categories
 
-        # self.input_2_hidden = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
         self.input_2_output = nn.GRU(n_categories + input_size, hidden_size)
         self.output_2_output = nn.Linear(n_categories + hidden_size, output_size)
+        self.hidden_2_hidden = nn.Linear(hidden_size, hidden_size)
         self.dropout = nn.Dropout(.1) # prevent over fitting / increase sampling variety
         self.softmax = nn.LogSoftmax(dim=1)
 
@@ -18,10 +18,14 @@ class RNN(nn.Module):
         input = input.view(1,1,self.input_size)
         category = category.view(1,1,self.category_size)
         input_combined = torch.cat((category, input), 2)
-        # hidden = self.input_2_hidden(input_combined)
+        
         output, hidden = self.input_2_output(input_combined, hidden)
+       
         output_combined = torch.cat((category, output), 2)
+        
+        hidden = self.hidden_2_hidden(hidden)
         output = self.output_2_output(output_combined)
+        
         output = self.dropout(output)
         output = output.view(1,-1)
         output = self.softmax(output)
